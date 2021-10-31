b0d1396fd6ace769f7c1a8dbf12b756f9e26a50a
Randy R. Martínez <55677075+randymxd06@users.noreply.github.com>
2021-10-30 22:09:52 -0400

    Vi los fundamentos de Vue.js
    En esta sección se vio:
    
    • Cómo crear un proyecto de Vue.js.
    
    • La estructura de directorios que se crea por defecto en Vue.js.
    
    • Cómo crear un componente.
    
    • Manejar el estado del componente.
    
    • Las propiedades computadas para guardar el estado en la caché.
    
    • Hicimos que se pueda incrementar y decrementar uno de los componentes.
    
    • Aprendimos a usar las props.
    
    • Las diferentes formas de definir las props.
    
    • A validar las props.

---
+++ vue-fundamentos-cli-fin-seccion-4/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  
+  <Counter :start="100" />
+
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
