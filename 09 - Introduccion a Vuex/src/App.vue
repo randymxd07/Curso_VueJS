00569bbda54aad033d1e987b53ced2e52881c09c
Randy Martinez <randym0624@gmail.com>
2022-04-30 22:43:54 -0400

    Vi la seccion de introduccion a vuex, aprendi sobre el state, las acciones, las mutaciones, los getters y aprendi a manejarlo por modulos

---
+++ 09 - Introduccion a Vuex/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Counter />
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Counter: defineAsyncComponent( () => import('@/components/Counter.vue') )
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 09 - Introduccion a Vuex/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import store from './store'
+
+
+import App from './App.vue'
+
+createApp(App)
+    .use( store )
+    .mount('#app')
